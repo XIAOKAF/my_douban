c292925f8c64b9efa1d810bf0347fd7577b7caef
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-03-04 21:51:42 +0800

    refactor:在token结构体中增加过期时间

--- model/token.go
+++ model/token.go
@@ -2,10 +2,12 @@ package model
 
 import (
 	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 type TokenClaims struct {
-	Mobile  string
-	Variety string //token与refreshToken两种类型
+	Mobile     string
+	Variety    string //token与refreshToken两种类型
+	ExpireTime time.Time
 	jwt.StandardClaims
 }
