e983a9688802a00a35c6bf3dc39a7ab1fb4fe150
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-03-19 21:26:28 +0800

    feat: 新增查询用户是否注册以及记住登录的功能

--- api/oAuth.go
+++ api/oAuth.go
@@ -6,6 +6,7 @@ import (
 	"gin/service"
 	"gin/tool"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func oAuth(ctx *gin.Context) {
@@ -32,13 +33,40 @@ func oAuth(ctx *gin.Context) {
 		return
 	}
 	//查询该用户是否注册过
-	err = service.CheckRegisterByUsername(user)
+	err, flag := service.CheckRegisterByUsername(user)
 	if err != nil {
 		fmt.Println("查询用户是否注册错误", err)
 		tool.ReturnFailure(ctx, 500, "授权注册或登录失败")
 		return
 	}
-	//授权接口中注册
-
+	//未查询到该用户
+	if !flag {
+		err = service.RegisterByOAuth(user)
+		if err != nil {
+			fmt.Println("注册错误", err)
+			tool.ReturnFailure(ctx, 500, "授权注册或登录失败")
+			return
+		}
+	}
 	//记住登录状态
+	//生成token,有效时间2分钟
+	LoginToken, err := service.CreatToken(user.Username, "token", 30)
+	if err != nil {
+		fmt.Println("token生成失败", err)
+		tool.ReturnFailure(ctx, 500, "服务器错误")
+		return
+	}
+	//生成refreshToken，有效时间24小时
+	refreshToken, err := service.CreatToken(user.Username, "refreshToken", 1440)
+	if err != nil {
+		fmt.Println("refreshToken生成失败", err)
+		tool.ReturnFailure(ctx, 500, "服务器错误")
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":         200,
+		"token":        LoginToken,
+		"refreshToken": refreshToken,
+	})
 }
