84e5f0c6aafaf70ab64cbb023d3e5c331b769cd9
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-03-19 21:27:30 +0800

    feat: 新增查询用户是否已经注册的功能

--- dao/oAuth.go
+++ dao/oAuth.go
@@ -1,1 +1,25 @@
 package dao
+
+import "gin/model"
+
+func CheckRegisterByUserName(user model.User) error {
+	var userId int
+	rows := DB.QueryRow("SELECT userId FROM user WHERE username = ? ", user.Username)
+	if rows.Err() != nil {
+		return rows.Err()
+	}
+	err := rows.Scan(&userId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func RegisterByOAuth(user model.User) error {
+	sql := "insert into user(username,password)values(?,?)"
+	_, err := DB.Exec(sql, user.Username, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
